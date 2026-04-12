3da2d550d22a52c2a2355bfeff46d852cbc376ed
dejung71020@gmail.com <dejung71020@gmail.com>
2026-04-12 13:26:27 +0900

    etc: 모델 객체 단수형 리포지토리에서 변경

--- app/domains/transactions/repository.py
+++ app/domains/transactions/repository.py
@@ -1,11 +1,11 @@
 # Path: app/domains/transactions/repository.py
 from sqlalchemy.orm import Session
 from uuid import UUID
-from .models import Transactions
+from .models import Transaction
 
 class TransactionsRepository:
     def __init__(self, db: Session):
         self.db = db
 
     def get_by_id(self, obj_id: UUID):
-        return self.db.query(Transactions).filter(Transactions.id == obj_id).first()
+        return self.db.query(Transaction).filter(Transaction.id == obj_id).first()
