992c41f7a398ddb181f0ad4ff62eed3a5fbbe5f2
LDJ <dejung71020@gmail.com>
2026-04-12 13:36:15 +0900

    etc: lifespan으로 테이블 개발자모드 삭제코드 작성

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -16,7 +16,10 @@ async def lifespan(app: FastAPI):
     logger.info(f"[{settings.PROJECT_NAME}] [{settings.VERSION}] 서버 실행")
 
     # PostgreSQL DB 테이블 생성
-    try:        
+    try:
+        if settings.DEBUG:
+            logger.info("LIFESPAN: DEBUG MODE\n 전체 테이블 삭제")
+            Base.metatdata.drop_all(bind=engine)        
         Base.metadata.create_all(bind=engine)
         logger.info("DB 테이블 생성 완료")
     except Exception as e:
