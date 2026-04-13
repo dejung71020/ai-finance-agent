570efa0df863ca725650e6c0659accac655a1049
dejung71020@gmail.com <dejung71020@gmail.com>
2026-04-13 10:40:55 +0900

    1.4.2: 2- 오류로 수정이 commit이 안됨

--- app/api/router.py
+++ app/api/router.py
@@ -6,16 +6,16 @@ from app.domains.planning.router import router as planning_router
 from app.domains.automation.router import router as automation_router
 from app.domains.investment.router import router as investment_router
 from app.domains.coach.router import router as coach_router
-from app.domains.transaction.router import router as transactions_router
-from app.domains.assets.router import router as assets_router
-from app.domains.users.router import router as users_router
+from app.domains.transaction.router import router as transaction_router
+from app.domains.asset.router import router as asset_router
+from app.domains.users.router import router as user_router
 
 router = APIRouter(prefix="/api")
 
 # --- ROUTERS START ---
-router.include_router(users_router)
-router.include_router(assets_router)
-router.include_router(transactions_router)
+router.include_router(user_router)
+router.include_router(asset_router)
+router.include_router(transaction_router)
 router.include_router(coach_router)
 router.include_router(investment_router)
 router.include_router(automation_router)
